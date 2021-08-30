1185e3bbd9f1e40cc34fe66d04e4891c77603705
schop-rob <roschoeppner@gmail.com>
2021-08-30 10:38:14 +0200

    bessere Zufallsvariablen

--- ABTreeTaskGenerator.py
+++ ABTreeTaskGenerator.py
@@ -5,15 +5,18 @@ parser.add_argument("--tasks", default = 15, help = "Anzahl Aufgaben, die generi
 args = parser.parse_args()
 
 numbers = []
+adder = 0
 print("Starte mit leerem (a,b)-Baum")
 for i in range(int(args.tasks)):
-    value = bool(random.getrandbits(1))
-    if (value | len(numbers) < 2):
+    value = random.randrange(16)
+    if (value + adder < 11 or len(numbers) < 2):
         newNum = random.randrange(70)
         while newNum in numbers:
             newNum = random.randrange(70)
         numbers.append(newNum)
         print(f"insert({newNum})")
+        adder = 0
     else :
         numba = numbers.pop(random.randrange(len(numbers)))
         print(f"delete({numba})")
+        adder = 3
